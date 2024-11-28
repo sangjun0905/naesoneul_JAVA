415cce959c02d8a10b98b241b93a04ce42907e8b
sangjun0905 <hyunsj090565@gmail.com>
2024-11-28 17:09:04 +0900

    datatype

--- hello/helloworldapp.java
+++ hello/helloworldapp.java
@@ -1,5 +1,21 @@
-public class helloworldapp{ //파일과 동일한 이름의 클래스를 찾음.
-	public static void main(String[] args) {
-		System.out.println("helloworld33332!!");
-	}
+import javax.swing.*;   
+import java.awt.Dimension;
+import java.awt.Toolkit;
+public class helloworldapp{
+    public static void main(String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("HelloWorld GUI");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setPreferredSize(new Dimension(800, 300));
+                JLabel label = new JLabel("Hello World!!", SwingConstants.RIGHT);
+                frame.getContentPane().add(label);
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setLocation(dim.width/2-400/2, dim.height/2-300/2);
+
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
 }

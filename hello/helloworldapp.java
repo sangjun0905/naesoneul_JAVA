66b52fd7513b724852afafb086611d8c381a5c69
sangjun0905 <hyunsj090565@gmail.com>
2024-11-28 12:43:40 +0900

    test2

--- hello/helloworldapp.java
+++ hello/helloworldapp.java
@@ -1,5 +1,5 @@
 public class helloworldapp{ //파일과 동일한 이름의 클래스를 찾음.
 	public static void main(String[] args) {
-		System.out.println("helloworld22!!");
+		System.out.println("helloworld33332!!");
 	}
 }
